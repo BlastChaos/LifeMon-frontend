1d09bc5a4dcd2b3a4afe77522686272f4fe44a1a
PequeKiki <orellanaabregoeric@gmail.com>
2025-02-01 16:45:03 -0500

    login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,32 +3,41 @@ import { useNavigate } from 'react-router';
 export const Login: React.FC = () => {
   const navigation = useNavigate();
   return (
-    <Stack
-      bg="var(--mantine-color-body)"
-      align="stretch"
-      justify="center"
-      gap="md"
+      <Stack
+        align="stretch"
+        justify="center"
+        gap="md"
+        style={{ 
+          background: 'rgba(255, 255, 255, 0.8)', 
+          padding: '20px', 
+          borderRadius: '10px', 
+          maxWidth: '400px', 
+          width: '100%' 
+        }}
       >
-      <Title order={6}>Welcome to</Title>
-      <Title order={1}>Lifemon !!!</Title>
-      <TextInput
-        placeholder="Username"
-        label="Your username"
-        radius="xl"
-        size="md"
-        required
-      />
-      <TextInput
-        placeholder="Password"
-        label="Your password"
-        radius="xl"
-        size="md"
-        required
-      />
-      <Button variant="filled" color="indigo" size="xl" radius="lg" onClick={() => navigation("/home")} >CONNECT</Button>
+        <Title order={6}>Welcome to</Title>
+        <Title order={1}>Lifemon !!!</Title>
+        <Text>Welcome to a world where everything can be alive and fight for you</Text>
 
-      <Text>Welcome to world who everything can be alive and fight for you</Text>
-      
-    </Stack>
+        <Stack justify="right">
+          <TextInput
+            placeholder="Username"
+            label="Your username"
+            radius="xl"
+            size="md"
+            required
+          />
+          <TextInput
+            placeholder="Password"
+            label="Your password"
+            radius="xl"
+            size="md"
+            required
+          />
+
+        </Stack>
+       
+        <Button variant="filled" color="indigo" size="xl" radius="lg" onClick={() => navigation("/home")}>CONNECT</Button>
+      </Stack>
   );
 };
