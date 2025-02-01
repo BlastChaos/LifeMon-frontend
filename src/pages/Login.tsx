53cb6fd8f2e4a6130cb4470022a3c799cdcfc458
PequeKiki <orellanaabregoeric@gmail.com>
2025-02-01 13:32:05 -0500

    Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,18 @@
+import { Stack, Button, Title } from '@mantine/core';
+import { useNavigate } from 'react-router';
 export const Login: React.FC = () => {
+  const navigation = useNavigate();
   return (
-    <>
-      <div>HELLO WORLDD</div>
-      <h1>Vite + React</h1>
-    </>
+    <Stack
+      h={300}
+      bg="var(--mantine-color-body)"
+      align="stretch"
+      justify="center"
+      gap="md"
+      >
+      <Title order={6}>Welcome to</Title>
+      <Title order={1}>Lifemon !!!</Title>
+      <Button variant="filled" color="indigo" size="xl" radius="lg" onClick={() => navigation("/home")} >CONNECT</Button>
+    </Stack>
   );
 };
