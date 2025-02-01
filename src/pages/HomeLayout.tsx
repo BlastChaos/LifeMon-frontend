dbb0c6819dc2851f3ea3a7ed040537909eb9787d
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2025-02-01 12:55:03 -0500

    add

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,1 +1,40 @@
-export const HomeLayout: React.FC = () => {};
+import { AppShell, Burger, Group } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Outlet } from "react-router";
+
+export const HomeLayout: React.FC = () => {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="sm"
+          />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md"></AppShell.Navbar>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
+  );
+};
