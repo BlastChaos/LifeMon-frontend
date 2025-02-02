e7c1332345ef2d76e44afdc585edb7a4f5f6cb60
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2025-02-02 07:27:00 -0500

    close

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -1,30 +1,24 @@
 import { Stack, Tabs } from "@mantine/core";
-import { MyTeam } from "./MyTeam"; 
-import { LifemonList } from "./LifemonList"; 
+import { MyTeam } from "./MyTeam";
+import { LifemonList } from "./LifemonList";
 
 export const Team: React.FC = () => {
   return (
-    <Stack justify="center">
-     <Tabs variant="pills" defaultValue="gallery">
-      <Tabs.List>
-        <Tabs.Tab value="team">
-          Team
-        </Tabs.Tab>
-        <Tabs.Tab value="lifemon">
-          Lifemon
-        </Tabs.Tab>
-      </Tabs.List>
+    <Stack>
+      <Tabs variant="pills" defaultValue="team">
+        <Tabs.List defaultValue={"team"}>
+          <Tabs.Tab value="team">Team</Tabs.Tab>
+          <Tabs.Tab value="lifemon">Lifemon</Tabs.Tab>
+        </Tabs.List>
 
-      <Tabs.Panel value="team">
-        <MyTeam/>
-      </Tabs.Panel>
+        <Tabs.Panel value="team">
+          <MyTeam />
+        </Tabs.Panel>
 
-      <Tabs.Panel value="lifemon">
-        <LifemonList/>
-      </Tabs.Panel>
-    </Tabs>      
+        <Tabs.Panel value="lifemon">
+          <LifemonList />
+        </Tabs.Panel>
+      </Tabs>
     </Stack>
-     
-
   );
 };
